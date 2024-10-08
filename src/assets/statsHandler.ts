a1279bb2fe4cc81e7099e116ef1cd3583fdf2a1f
NadavMozeson <nadavson02@gmail.com>
2024-10-08 23:04:50 +0300

    Update statsHandler.ts

--- src/assets/statsHandler.ts
+++ src/assets/statsHandler.ts
@@ -6,11 +6,9 @@ import { Guild, VoiceChannel } from "discord.js";
 export const updateStats = withErrorHandling(async () => {
     await updateDiscordStats()
     await updateYouTubeStats()
-    await updateVIPAmountCounter()
     setInterval(async () => {
         await updateYouTubeStats()
         await updateDiscordStats()
-        await updateVIPAmountCounter()
     }, 3 * 60 * 60 * 1000); 
 })
 
